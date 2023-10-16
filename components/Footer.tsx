98ac6c513700b28041cc586964cefbc657e41682
dev-from-cbd <veryfatwombat@gmail.com>
2023-10-17 06:39:40 +1030

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flex flex-col text-black-100 mt-5 border-gray-100">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16">
+        Footer
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
